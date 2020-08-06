993a3e0d64edf575fc4e6b2cb74d2b9147396016
shin-beginner <nan5mo2@yahoo.co.jp>
2020-08-06 16:49:18 -0700

    .

--- tdl/urls.py
+++ tdl/urls.py
@@ -5,6 +5,7 @@ app_name = 'tdl'
 
 urlpatterns = [
   path('', views.IndexView.as_view(), name='index'),
+  path('execution/', views.ExecutionView.as_view(), name='execution'),
   path('create/', views.CreateView.as_view(), name='create'),
   path('<int:pk>/update/', views.UpdateView.as_view(), name='update'),
   path('<int:pk>/delete/', views.DeleteView.as_view(), name='delete'),
--- tdl/views.py
+++ tdl/views.py
@@ -1,9 +1,23 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from .models import Tdl
+import random
 
 class IndexView(generic.ListView):
    model = Tdl
+
+class ExecutionView(generic.ListView):
+  model = Tdl
+  template_name = 'tdl/tdl_execution.html'
+
+  def get_queryset(self):
+#    return Tdl.objects.order_by('?')[:1]
+    pk = Tdl.objects.values_list('pk', flat=True)
+    pk_list = list(pk)
+    pk_random = random.choice(pk_list)
+    queryset = Tdl.objects.filter(pk=pk_random)
+
+    return queryset
 
 class CreateView(generic.edit.CreateView):
   model = Tdl
